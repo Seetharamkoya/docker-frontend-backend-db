2f80a02491e2f5ca26c1623dc1ad0c4e8ac1a5f6
seetharamkoya <seetharamkoya9@gmail.com>
2023-07-05 14:59:04 +0530

    provision infra

---
+++ aws/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-04e601abe3e1a910f"
+  instance_type = "t2.micro"
+  key_name      = "Docker-aws"
+  vpc_security_group_ids = [
+    aws_security_group.re-ex.id
+  ]
+
+  tags = {
+    Name = "React-express-instance"
+  }
+}
+
+# Create a security group 
+resource "aws_security_group" "re-ex" {
+  name = "re-ex"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
